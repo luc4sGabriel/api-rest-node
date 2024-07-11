407683c22ba21e7f42448d1d2031c3cc97f0a2f4
luc4sGabriel <lucas.g4briel.04@gmail.com>
2024-07-11 12:33:08 -0300

    Create First Migration

---
+++ db/migrations/20240711152758_create-documents.ts
@@ -0,0 +1,7 @@
+import type { Knex } from 'knex'
+
+// eslint-disable-next-line
+export async function up(knex: Knex): Promise<void> {}
+
+// eslint-disable-next-line
+export async function down(knex: Knex): Promise<void> {}
